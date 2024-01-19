6a6880d3b1899f5f4605fb0837e32f63b86a556b
Guillermo Cardoze <vital_tmv@yahoo.com>
2024-01-18 19:34:56 -0600

    html update 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://www.freetogame.com/api/games')
+    fetch('http://localhost:3000/games')
     .then(resp => resp.json())
     .then(data => console.log(data))
 })
