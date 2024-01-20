206ad78b8733a60b8cc0366ca05e8d8ba4a8988d
Guillermo Cardoze <vital_tmv@yahoo.com>
2024-01-20 09:35:36 -0600

    fetch and img display

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetch('http://localhost:3000/games')
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(games => renderGames(games))
 })
+
+const gameList = document.getElementById('all-games')
+const gameImg = document.getElementById('detail-image')
+const gameName = document.getElementById('detail-name')
+const gameDescript = document.getElementById('detail-description')
+const gameDev = document.getElementById('developer-display')
+// console.log(gameDev)
+
+const renderGames = games => {
+    games.forEach(renderGame)
+}
+
+const gameAddHandler = game => {
+    gameImg.src = game.thumbnail
+    gameName.textContent = game.title
+    gameDescript.textContent = game.short_description
+    gameDev.textContent = game.developer
+    
+}
+
+const renderGame = game => {
+    const img = document.createElement('img')
+    img.src = game.thumbnail;
+    img.addEventListener('click', () => gameAddHandler(game))
+    
+
+    gameList.append(img)
+}
+
